14fc0fa6d36e70fe8126c2677bd17fc33bf3babc
Brady Vercher <brady@blazersix.com>
2014-10-07 08:19:49 -0700

    Move the timepicker script into a vendor directory.

--- includes/class-audiotheme.php
+++ includes/class-audiotheme.php
@@ -192,7 +192,7 @@ class AudioTheme {
 
 		wp_register_script( 'audiotheme',               $base_url . 'includes/js/audiotheme.js',               array( 'jquery', 'audiotheme-media-classes' ), AUDIOTHEME_VERSION, true );
 		wp_register_script( 'audiotheme-media-classes', $base_url . 'includes/js/audiotheme-media-classes.js', array( 'jquery' ), AUDIOTHEME_VERSION, true );
-		wp_register_script( 'jquery-timepicker',        $base_url . 'includes/js/jquery.timepicker.min.js',    array( 'jquery' ), '1.1', true );
+		wp_register_script( 'jquery-timepicker',        $base_url . 'includes/js/vendor/jquery.timepicker.min.js',    array( 'jquery' ), '1.1', true );
 
 		wp_register_style( 'audiotheme', $base_url . 'includes/css/audiotheme.min.css' );
 	}
