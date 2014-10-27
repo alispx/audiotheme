dd870062fdd4702d9edbcb39e4d26a30bcea2ec3
Brady Vercher <brady@blazersix.com>
2014-10-27 15:27:55 -0700

    Introduce intial track list functionality.

--- includes/js/audiotheme.js
+++ includes/js/audiotheme.js
@@ -1,7 +1,24 @@
+/*global AudiothemeTracks:false */
+
 (function( window, $, undefined ) {
 	'use strict';
 
 	$( document ).ready(function() {
+		var $tracklist = $( '.audiotheme-tracklist-section' );
+
+		if ( $tracklist.length && $.isFunction( $.fn.cuePlaylist ) ) {
+			$tracklist.cuePlaylist({
+				cuePlaylistTracks: AudiothemeTracks.record,
+				cueSelectors: {
+					playlist: '.audiotheme-tracklist-section',
+					track: '.audiotheme-track',
+					trackCurrentTime: '.jp-current-time',
+					tracklist: '.audiotheme-tracklist'
+				},
+				features: ['cueplaylist']
+			});
+		}
+
 		$( '.js-media-classes' ).audiothemeMediaClasses({
 			breakpoints: [ 600, 400 ]
 		});
