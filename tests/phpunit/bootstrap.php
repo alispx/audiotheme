fe1e96b890cb8b0ecbab7d554ed634a6b310fd5d
Brady Vercher <brady@blazersix.com>
2015-03-29 11:38:01 -0700

    Update the unit testing setup.

---
+++ tests/phpunit/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+		$_tests_dir = getenv( 'WP_DEVELOP_DIR' ) . 'tests/phpunit';
+	} elseif ( file_exists( '../../../../../tests/phpunit/includes/bootstrap.php' ) ) {
+		$_tests_dir = '../../../../../tests/phpunit';
+	} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
+		$_tests_dir = '/tmp/wordpress-tests-lib';
+	}
+}
+
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins'  => array(
+		'audiotheme/audiotheme.php',
+	),
+	'timezone_string' => 'America/Los_Angeles',
+);
+
+require_once $_tests_dir . '/includes/functions.php';
+
+tests_add_filter( 'muplugins_loaded', function() {
+	require( dirname( dirname( __DIR__ ) ) . '/audiotheme.php' );
+} );
+
+require $_tests_dir . '/includes/bootstrap.php';
